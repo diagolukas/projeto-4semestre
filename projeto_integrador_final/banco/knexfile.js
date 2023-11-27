7a153b2bed084143dc55f73afc0bdd5391ad12e2
Roberto Lukas Pereira Diago <108032735+diagolukas@users.noreply.github.com>
2023-11-27 20:48:14 -0300

    Add files via upload

---
+++ projeto_integrador_final/banco/knexfile.js
@@ -0,0 +1,12 @@
+export const development = {
+    client: 'mysql2',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : '',
+      database : 'senatec'
+    },
+    useNullAsDefault: true
+  }
+  
